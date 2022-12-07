d70c4fb40242ca17192a59910fe821d013834458
louistanntt <louistanntt@gmail.com>
2022-12-07 16:28:37 +0700

    !chore: doing set up

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.tsx to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+export {default} from './storybook'; 
 
 const styles = StyleSheet.create({
   container: {
